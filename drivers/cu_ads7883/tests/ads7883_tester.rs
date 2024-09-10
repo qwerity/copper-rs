82b6e461f2174b8a7777ebb44ec41ae61d302769
Guillaume Binet <gbin@gootz.net>
2024-09-10 15:58:58 -0500

    remove unused testing code

--- drivers/cu_ads7883/tests/ads7883_tester.rs
+++ drivers/cu_ads7883/tests/ads7883_tester.rs
@@ -1,37 +1,11 @@
-use cu29::clock::RobotClock;
-use cu29::config::NodeInstanceConfig;
-use cu29::cutask::{CuMsg, CuSinkTask, CuTaskLifecycle, Freezable};
-use cu29::CuResult;
+use cu29::cutask::{CuSinkTask, CuTaskLifecycle};
 use cu29_derive::copper_runtime;
 use cu29_helpers::basic_copper_setup;
 use cu29_log_derive::debug;
-use cu_ads7883::ADSReadingMsg;
 use std::path::PathBuf;
 
 #[copper_runtime(config = "tests/copperconfig.ron")]
 struct ADS78883Tester {}
-
-pub struct ADS78883TestSink {}
-
-impl Freezable for ADS78883TestSink {}
-
-impl CuTaskLifecycle for ADS78883TestSink {
-    fn new(_config: Option<&NodeInstanceConfig>) -> CuResult<Self>
-    where
-        Self: Sized,
-    {
-        Ok(Self {})
-    }
-}
-
-impl CuSinkTask for ADS78883TestSink {
-    type Input = ADSReadingMsg;
-
-    fn process(&mut self, _clock: &RobotClock, new_msg: &mut CuMsg<Self::Input>) -> CuResult<()> {
-        debug!("Received: {}", &new_msg.payload);
-        Ok(())
-    }
-}
 
 #[test]
 fn test_driver_with_hardware() {
